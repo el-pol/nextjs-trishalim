476122251c06ec06a0d0c9590b415b999fc08119
Trisha <trisha.l@apolinargroup.com>
2021-11-13 17:02:55 +0700

    change nav colors

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,12 +26,14 @@ export const Header = (): ReactElement => {
   ];
 
   return (
-    <div className="w-full bg-pink sticky top-0 hidden md:block shadow-sm">
+    <div className="w-full bg-gray-100 sticky top-0 hidden md:block shadow bg-opacity-80" style={{
+      backdropFilter: `saturate(180%) blur(20px)`
+    }}>
       <div className="container px-5 py-4 mx-auto max-w-3xl text-center">
       {
         links.map((link) => (
           <Link href={link.url} key={link.url}>
-            <a href={link.url} className="text-white uppercase p-5 opacity-80 hover:opacity-100 tracking-wider text-xs">{link.title}</a>
+            <a href={link.url} className="text-pink font-semibold uppercase p-5 opacity-80 hover:opacity-100 tracking-wider text-xs">{link.title}</a>
           </Link>
         ))
       }
