5565f852e1616e5f6c437e80c5bbd14756c6da56
Trisha <trisha.l@apolinargroup.com>
2021-11-14 08:52:52 +0700

    add new post: CSS for Web3

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,6 +168,11 @@ const Home: NextPage = () => {
 
   const content = [
     {
+      title: 'CSS Tricks to Create that Dark Futuristic Web3 Look',
+      imageUrl: '/content-css-for-web3.jpg',
+      link: 'https://dev.to/trishathecookie/css-tricks-to-create-that-dark-futuristic-web3-look-53bm',
+    },
+    {
       title: 'React: How to create a reusable form using React Context',
       imageUrl: '/content-react-context.png',
       link: 'https://dev.to/trishathecookie/react-creating-a-reusable-form-using-react-context-5eof'
