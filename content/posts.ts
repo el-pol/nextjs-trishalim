e015a5b62fbf4027028c2d6c0ce9aeefe4e64afe
Trisha <trisha.l@apolinargroup.com>
2022-02-11 14:12:23 +0700

    add phangan blog post

--- content/posts.ts
+++ content/posts.ts
@@ -1,5 +1,10 @@
 export const posts = [
     {
+        title: 'Digital Nomad Guide for Koh Phangan, Thailand',
+        imageUrl: 'https://blog.trishalim.com/content/images/2022/02/IMG_0140-1.jpeg',
+        link: 'https://blog.trishalim.com/digital-nomad-guide-for-koh-phangan-thailand/'
+    },
+    {
         title: 'CSS Tricks to Create that Dark Futuristic Web3 Look',
         imageUrl: '/content-css-for-web3.jpg',
         link: 'https://dev.to/trishathecookie/css-tricks-to-create-that-dark-futuristic-web3-look-53bm',
