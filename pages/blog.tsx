9b5e32304f3bce4d57d89a8586788a804ff2ae4f
Trisha <trisha.l@apolinargroup.com>
2022-02-23 10:20:49 +0700

    blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,22 @@
+import SiteLayout from "../components/SiteLayout";
+import React from "react";
+import {posts} from "../content/posts";
+import {Content} from "../components/Content";
+
+const Blog = () => {
+    return (
+        <SiteLayout>
+            <main className="container py-10 lg:py-16">
+                <h2 className="mb-8">Blog</h2>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
+                    {
+                        posts.map((c) => <div key={c.link}><Content content={c}/>
+                        </div>)
+                    }
+                </div>
+            </main>
+        </SiteLayout>
+    )
+}
+
+export default Blog
