35ac5c19ff8b43611328c3d08a177ba36a75186b
Trisha <trisha.l@apolinargroup.com>
2022-03-02 10:41:52 +0700

    add blog post

--- content/posts.ts
+++ content/posts.ts
@@ -2,6 +2,11 @@ import Post from "../types/post";
 
 export const posts: Array<Post> = [
   {
+    title: "How to Make a Web Developer Portfolio in 2022",
+    imageUrl: "/blog-how-to-make-a-web-developer-portfolio.png",
+    link: "https://dev.to/trishathecookie/how-to-create-a-portfolio-website-that-converts-for-software-developers-2a5h"
+  },
+  {
     title: "Digital Nomad Guide for Koh Phangan, Thailand",
     imageUrl:
       "https://blog.trishalim.com/content/images/2022/02/IMG_0140-1.jpeg",
