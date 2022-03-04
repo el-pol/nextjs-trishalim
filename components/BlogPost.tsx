00a4b8fd3793628135533420ed78c980edd5e152
Trisha <trisha.l@apolinargroup.com>
2022-03-04 15:48:04 +0700

    optimize images on blog post thumbnail

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,8 +1,38 @@
 import React, { ReactElement } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import Icon from "./Icon";
 import { PostOrPage } from "@tryghost/content-api";
+
+const getSrcSet = (url: string) => {
+  if (!url) return undefined;
+
+  const sizes = [
+    { imageWidth: 300, viewportWidth: 300 },
+    { imageWidth: 600, viewportWidth: 600 },
+    { imageWidth: 800, viewportWidth: 1000 },
+    { imageWidth: 800, viewportWidth: 2000 },
+  ];
+
+  if (url.startsWith("https://images.unsplash.com")) {
+    return sizes
+      .map(
+        (size) =>
+          `${url.replace("w=2000", `w=${size.imageWidth}`)} ${
+            size.viewportWidth
+          }w`
+      )
+      .join(", ");
+  }
+
+  return sizes
+    .map(
+      (size) =>
+        `${url.replace("images", `images/size/w${size.imageWidth}`)} ${
+          size.viewportWidth
+        }w`
+    )
+    .join(", ");
+};
 
 const Post = (props: { content: PostOrPage }): ReactElement => {
   const { feature_image, title, slug } = props.content;
@@ -12,7 +42,12 @@ const Post = (props: { content: PostOrPage }): ReactElement => {
       <a>
         <div className="relative w-full h-60 sm:h-60 md:h-44 rounded mb-5 overflow-hidden">
           {feature_image && (
-            <Image src={feature_image} layout="fill" objectFit="cover" alt="" />
+            <img
+              src={feature_image}
+              srcSet={getSrcSet(feature_image)}
+              alt=""
+              className="object-cover"
+            />
           )}
           <div className="absolute top-0 left-0 w-full h-full bg-primary rounded opacity-0 hover:opacity-80 transition-all duration-500 flex items-center justify-center">
             <Icon
